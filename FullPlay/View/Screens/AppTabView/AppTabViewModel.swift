e890f50258f9e3bb9c89eafe4d899600f93eefc0
Henry Bravo <hbravo@tryurapp.com>
2022-02-04 11:38:21 -0500

    Add AppTabView

---
+++ FullPlay/View/Screens/AppTabView/AppTabViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AppTabViewModel.swift
+//  FullPlay
+//
+//  Created by Henry Bravo on 2/4/22.
+//
+
+import SwiftUI
+
+extension AppTabView {
+    
+    final class AppTabViewModel: ObservableObject {
+        
+        @Published var isShowingOnboardView = false
+        @AppStorage("hasSeenOnboardView") var hasSeenOnboardView = false {
+            didSet { isShowingOnboardView = hasSeenOnboardView }
+        }
+        
+        let kHasSeenOnboardView = "hasSeenOnboardView"
+        
+        
+        func checkIfHasSeenOnboard() {
+            if !hasSeenOnboardView { hasSeenOnboardView = true }
+        }
+    }
+}
